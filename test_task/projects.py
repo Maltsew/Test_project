5f3d8314e02ddc55a978ec656ad6e37946b572ba
Maltsew <87392631+Maltsew@users.noreply.github.com>
2021-08-19 17:24:57 +0300

    Update projects.py
    commit

--- test_task/projects.py
+++ test_task/projects.py
@@ -12,6 +12,8 @@
 с хранилища, архивировать такой каталог и после его отправлять,
 можно поработать с потоками.
 """
+
+
 import paramiko
 import os
 from stat import S_ISDIR as isdir
@@ -23,16 +25,12 @@ import logging.config
 
 
 def download_from_remote(sftp_obj, remote_dir_name, local_dir_name):
-    """доступ к файлу получаем также, как это делает встроенная библиотека FTP,
-    но, отличие sFTP от FTP в том, что первый имеет
-    встроенную защиту и обмен похож на обмен по SSH. SFTP можно поставить
-    отдельно, но в библиотеке paramiko он уже реализован"""
     remote_file = sftp_obj.stat(remote_dir_name)
     if isdir(remote_file):
         #если remote - каталог - идем идем глубже, папки скачивать нельзя
         #создаем такую же папку на локальном компьютере, если ее нет
         check_local_dir(local_dir_name)
-        print('загрузка папки: ' + remote_dir_name)
+        #print('загрузка папки: ' + remote_dir_name)
         for remote_file_name in sftp.listdir(remote_dir_name):
             sub_remote = os.path.join(remote_dir_name, remote_file_name)
             sub_remote = sub_remote.replace('\\', '/')
@@ -43,21 +41,22 @@ def download_from_remote(sftp_obj, remote_dir_name, local_dir_name):
         """исходная папка not isdir - конечный каталог, 
         день месяца. загружаем файлы, путь на локальном такой же как до
         исходной папки на сервере"""
-        print ('загрузка файла: '+ remote_dir_name)
+        
+        #print ('загрузка файла: '+ remote_dir_name)
         sftp.get(remote_dir_name, local_dir_name)
         logger = logging.getLogger("exampleApp")
         logger.info('download file with name' + f'{remote_dir_name}')
         
 
 def check_local_dir(local_dir_name):
-    """проверка локальной папки, если нет - создаем"""
+    #проверка локальной папки, если нет - создаем
     if not os.path.exists(local_dir_name):
         os.makedirs(local_dir_name)
         logger = logging.getLogger("exampleApp")
         logger.info(f'{local_dir_name} folder created')
         
 def create_archive(local_dir_name):
-    """ архивирование ZipFile"""
+    #архивирование ZipFile
     zf = zipfile.ZipFile(local_dir_name, "w")
     for dirname, subdirs, files in os.walk(local_dir_name):
         zf.write(dirname)
@@ -67,7 +66,7 @@ def process_logging():
     logging.config.fileConfig('logging.conf')
         
 if __name__ == "__main__":
-    """"""
+    
     process_logging()
     logger = logging.getLogger("exampleApp")
     logger.info('Starting program')
@@ -77,24 +76,34 @@ if __name__ == "__main__":
     storage_user_name = 'USER'
     storage_password = 'PASS'
     storage_port = 22
+    
+    
     """Резберемся теперь со storage. Загрузку данных с него необходимо выполнить
     только если диск занят на > 90% (примем что диск один) или если данные
     хранятся на нем больше 90 дней. Можем получить эту информацию по ssh
     сервера, и тут снова использую paramiko, т.к. с его помощью можно выполнить
     запрос напрямую"""
+    
+    
     client = paramiko.SSHClient()
     client.connect(storage_name, storage_port, 
                    storage_user_name, storage_password)
     stdin, stdout, stderr = client.exec_command('df -h')
+    
+    
     """в stdout получим колонки Filesystem, Size, Used, Avail, Use%, Mounted on
     получим только значение USE%, потом отбросим %, приведем к int и сравним
     со значением 90. Превысили - начинаем выгрузку со storage. На сервере может
     быть несколько систем, много дисков, как вариант в stdout считывать [4] для
     всех строк, потом посчитать среднюю заполненность сервера, думаю"""
+    
+    
     current_volume = stdout.sys.agrv[4]
     procent = '%'
     current_volume = current_volume - procent
     current_volume = int(current_volume)
+    
+    
     """ Для второго условия выгрузки могу только предположить. также через
     SSHClient заходить на сервер. сделать функцию, которая будет проходить по
     всем папкам,которые являются дерикториями, и записывать названия в 
@@ -104,13 +113,19 @@ if __name__ == "__main__":
     это и будут папки с датами старше 90 дней. Скорее всего, все можно сделать
     намного проще в линуксе, надо подумать
     """
+    
+    
     if current_volume > 90:
         #путь к каталогу на локальной машине
         local_dir = 'archive/'
+        
+        
         """Путь к удаленному серверу. Требуется абсолютный путь к файлам, поскольку 
         папок много, нужно будет менять абсолютный путь для каждой даты
         решение - использовать pathlib, списковым включением верну все подкаталоги
         """
+        
+        
         p = Path('storage/')
         storage_remote_dir = [folder for folder in p.iterdir() if folder.is_dir()]
         #подключение к storage
@@ -132,8 +147,12 @@ if __name__ == "__main__":
         arhive_user_name = 'USER'
         arhive_password = 'PASS'
         arhive_port = 25
+        
+        
         """путь к архиву.на локальной машине надо перебирать из списка всех архивов
         отпрвляем только архивы, чтобы не скинуть лог"""
+        
+        
         #Путь к архивному серверу
         archive_remote_dir = 'archive/'
         rootPath = r"C:\local_archive"
